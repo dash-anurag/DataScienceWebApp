663a5f89ae74ab6e156a4b96d64ae97e9bb97ca7
Anurag Dash <anuragjeypore1234@gmail.com>
2020-09-19 22:47:08 +0530

    final commit

--- app.py
+++ app.py
@@ -66,24 +66,23 @@ filtered = data[
     ]
 hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0, 60))[0]
 chart_data = pd.DataFrame({'minute': range(60), 'crashes': hist})
-fig = px.bar(chart_data, x='minute', y='crashes',hover_data=['minute', 'crashes'], height=400)
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
 st.write(fig)
 
 st.header("Top 5 dangerous streets by affected type")
-select = st.selectbox("Affected type of people",['Pedestrians','Cyclists','Motorists'])
+select = st.selectbox("Affected type of people", ['Pedestrians', 'Cyclists', 'Motorists'])
 
 if select == 'Pedestrians':
-    st.write(original_data.query("injured_pedestrians >= 1")[['on_street_name','injured_pedestrians']]
-             .sort_values(by=['injured_pedestrians'],ascending=False).dropna(how='any')[:5])
+    st.write(original_data.query("injured_pedestrians >= 1")[['on_street_name', 'injured_pedestrians']]
+             .sort_values(by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5])
 
 elif select == 'Cyclists':
-    st.write(original_data.query("injured_cyclists >= 1")[['on_street_name','injured_cyclists']]
-             .sort_values(by=['injured_cyclists'],ascending=False).dropna(how='any')[:5])
+    st.write(original_data.query("injured_cyclists >= 1")[['on_street_name', 'injured_cyclists']]
+             .sort_values(by=['injured_cyclists'], ascending=False).dropna(how='any')[:5])
 
 else:
-    st.write(original_data.query("injured_motorists >= 1")[['on_street_name','injured_motorists']]
-             .sort_values(by=['injured_motorists'],ascending=False).dropna(how='any')[:5])
-
+    st.write(original_data.query("injured_motorists >= 1")[['on_street_name', 'injured_motorists']]
+             .sort_values(by=['injured_motorists'], ascending=False).dropna(how='any')[:5])
 
 if st.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
